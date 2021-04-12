358d3f5be981bf4c74abc9093b38114d00096cdf
Yomelichatprimi <82420888+Yomelichatprimi@users.noreply.github.com>
2021-04-12 18:17:31 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBwakKgDCpTPlRQo79mBnRltaykL9vkxFI",
+  authDomain: "michatprivadoangel.firebaseapp.com",
+  projectId: "michatprivadoangel",
+  storageBucket: "michatprivadoangel.appspot.com",
+  messagingSenderId: "430113754047",
+  appId: "1:430113754047:web:63e2429d6c179bbd6f2f0a",
+  measurementId: "G-3QHGD24GHX"
+});
